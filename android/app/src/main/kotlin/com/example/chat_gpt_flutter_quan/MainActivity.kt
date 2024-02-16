a43eaf2cc8369432b4398b48eaf6585b4dee6cef
Dao Minh Quan <daominhquan176@gmail.com>
2024-02-17 01:27:56 +0700

    update flutter 3.19.0

--- android/app/src/main/kotlin/com/example/chat_gpt_flutter_quan/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/chat_gpt_flutter_quan/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.chat_gpt_flutter_quan
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
--- lib/service/app_controller.dart
+++ lib/service/app_controller.dart
@@ -2,7 +2,7 @@ import 'package:chat_gpt_flutter_quan/flavors.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app_version_checker/flutter_app_version_checker.dart';
+// import 'package:flutter_app_version_checker/flutter_app_version_checker.dart';
 import 'package:get/get.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:url_launcher/url_launcher_string.dart';
@@ -23,31 +23,31 @@ class AppController extends GetxService {
 
   void checkVersion() async {
     if (kIsWeb) return;
-    AppVersionChecker().checkUpdate().then((value) {
-      Get.log(value.toString());
-      Future.delayed(const Duration(seconds: 2), () {
-        if (value.canUpdate == true) {
-          Get.dialog(
-            WillPopScope(
-              onWillPop: () async => false,
-              child: AlertDialog(
-                title: const Text('Update Available'),
-                content: const Text('Please update to latest version'),
-                actions: [
-                  TextButton(
-                    onPressed: () {
-                      launchUrlString(value.appURL!);
-                    },
-                    child: const Text('Update'),
-                  ),
-                ],
-              ),
-            ),
-            barrierDismissible: false,
-          );
-        }
-      });
-    });
+    // AppVersionChecker().checkUpdate().then((value) {
+    //   Get.log(value.toString());
+    //   Future.delayed(const Duration(seconds: 2), () {
+    //     if (value.canUpdate == true) {
+    //       Get.dialog(
+    //         WillPopScope(
+    //           onWillPop: () async => false,
+    //           child: AlertDialog(
+    //             title: const Text('Update Available'),
+    //             content: const Text('Please update to latest version'),
+    //             actions: [
+    //               TextButton(
+    //                 onPressed: () {
+    //                   launchUrlString(value.appURL!);
+    //                 },
+    //                 child: const Text('Update'),
+    //               ),
+    //             ],
+    //           ),
+    //         ),
+    //         barrierDismissible: false,
+    //       );
+    //     }
+    //   });
+    // });
   }
 
   Future<void> fetchRemoteConfig() async {
